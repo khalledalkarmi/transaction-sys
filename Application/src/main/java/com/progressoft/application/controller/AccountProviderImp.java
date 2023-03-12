fd7a1f96d70610a35329e472a75bbeba792397d8
Khaled Alkarmy <khaled.alkarmy@progressoft.com>
2023-03-12 08:37:35 +0300

    Revert "add structure of project"
    This reverts commit 161f5eddabe761916a2072a5e7c44f4d5beda3fd.

--- Application/src/main/java/com/progressoft/application/controller/AccountProviderImp.java
+++ Application/src/main/java/com/progressoft/application/controller/AccountProviderImp.java
@@ -1,8 +1,8 @@
 package com.progressoft.application.controller;
 
 
-import com.progressoft.application.repository.AccountClient;
 import com.progressoft.model.Account;
+import com.progressoft.application.repository.AccountClient;
 import com.progressoft.model.Transaction;
 import com.progressoft.validator.AccountProvider;
 import feign.Feign;
@@ -11,16 +11,20 @@ import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
 import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@AllArgsConstructor
 public class AccountProviderImp implements AccountProvider {
-    private final AccountClient accountClient;
     public Account getAccount(Transaction transaction) {
+        AccountClient accountClient = Feign.builder().encoder(new GsonEncoder())
+                .client(new OkHttpClient())
+                .decoder(new GsonDecoder())
+                .logger(new Slf4jLogger(AccountClient.class))
+                .logLevel(Logger.Level.FULL)
+                .target(AccountClient.class, "http://localhost:9090/api/v1/accounts");
+
         Account accountByCustomerIdAndAccountNumber = accountClient.getAccountByCustomerIdAndAccountNumber(transaction.getCustomerId(), transaction.getAccountNumber());
         log.info(accountByCustomerIdAndAccountNumber.toString());
         return accountByCustomerIdAndAccountNumber;
