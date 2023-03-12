161f5eddabe761916a2072a5e7c44f4d5beda3fd
Khaled Alkarmy <khaled.alkarmy@progressoft.com>
2023-03-12 08:34:19 +0300

    add structure of project

--- Application/src/main/java/com/progressoft/application/config/Configurations.java
+++ Application/src/main/java/com/progressoft/application/config/Configurations.java
@@ -1,21 +1,38 @@
 package com.progressoft.application.config;
 
-import com.progressoft.event.EventPublisher;
+import com.progressoft.application.repository.AccountClient;
 import com.progressoft.repository.TransactionRepository;
 import com.progressoft.usecases.CreateTransaction;
 import com.progressoft.validator.AccountProvider;
 import com.progressoft.validator.CreateTransactionValidator;
+import feign.Feign;
+import feign.Logger;
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import feign.okhttp.OkHttpClient;
+import feign.slf4j.Slf4jLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Configurations {
     @Bean
-    public CreateTransaction createTransaction(CreateTransactionValidator createTransactionValidator, TransactionRepository transactionRepository , EventPublisher eventPublisher) {
-        return new CreateTransaction(createTransactionValidator, transactionRepository , eventPublisher);
+    public CreateTransaction createTransaction(CreateTransactionValidator createTransactionValidator, TransactionRepository transactionRepository) {
+        return new CreateTransaction(createTransactionValidator, transactionRepository);
     }
+
     @Bean
     public CreateTransactionValidator createTransactionValidator (AccountProvider accountProvider){
         return new CreateTransactionValidator(accountProvider);
+    }
+
+    @Bean
+    public AccountClient accountProvider(){
+        return Feign.builder().encoder(new GsonEncoder())
+                .client(new OkHttpClient())
+                .decoder(new GsonDecoder())
+                .logger(new Slf4jLogger(AccountClient.class))
+                .logLevel(Logger.Level.FULL)
+                .target(AccountClient.class, "http://localhost:9090/api/v1/accounts");
     }
 }
