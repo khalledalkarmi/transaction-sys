86fa754e37ca06474ec114e1d74cc1d92e63bae4
Khaled Alkarmy <khaled.alkarmy@progressoft.com>
2023-03-12 09:09:31 +0300

    clean dead code

--- Application/src/main/java/com/progressoft/application/exception/ControllerAdviser.java
+++ Application/src/main/java/com/progressoft/application/exception/ControllerAdviser.java
@@ -3,18 +3,12 @@ package com.progressoft.application.exception;
 import com.progressoft.exception.AccountNotFoundException;
 import com.progressoft.exception.InvalidTransactionException;
 import com.progressoft.exception.NotEnoughBalanceException;
-import com.progressoft.exception.Violation;
-import feign.FeignException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class ControllerAdviser extends ResponseEntityExceptionHandler {
